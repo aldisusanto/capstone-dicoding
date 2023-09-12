12d53de1996639e5b8b00ba857afef86f93dada4
aldisusanto <aldisusanto648@gmail.com>
2023-09-12 10:16:42 +0800

    Initial Commit

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -11,13 +11,7 @@ android {
         minSdk = 24
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+
     buildFeatures {
         viewBinding = true
         buildConfig = true
